d4ded18a624a926c00dde1d0519246df8b88d28a
Dionisio <benevidescursos@gmail.com>
2023-06-06 19:55:35 -0300

    fix: settings main home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,14 +10,15 @@ export const Home = () => {
       <div className={"min-h-screen w-screen mb-60"}>
         <Navbar />
         <Header />
-        <main className={"mt-12 h-screen"}>
-          <section className={"w-full max-w-[1100px] h-screen"}>
+        <main className={"mt-12 h-screen flex gap-4 container"}>
+          <section className={"w-1/4"}></section>
+          <section className={"w-3/4 max-w-[1100px] h-screen"}>
             <ul
               className={
                 "grid h-screen md:grid-cols-2 lg:grid-cols-3 w-full gap-5 overflow-auto justify-center"
               }
             >
-              {carros.map((car, index) => (
+              {carros.map((car) => (
                 <Card
                   id={car.id}
                   description={car.description}
@@ -27,7 +28,7 @@ export const Home = () => {
                   userName={car.userName}
                   value={car.value}
                   year={car.year}
-                  key={index}
+                  key={car.id}
                 />
               ))}
             </ul>
