c8e4cd38e61a074450f7d1fbee06127af68826d8
Dionisio <benevidescursos@gmail.com>
2023-06-07 15:51:19 -0300

    fix: position of pagination and footer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Card } from "../../components/Card";
 import { Header } from "../../components/Header";
 import { Navbar } from "../../components/Navbar";
-import { Footer } from "../../components/footer";
+import { Footer } from "../../components/Footer";
 import { SideBar } from "../../components/sideBar";
 import { carros } from "../../utility";
 import { SideBarMobile } from "../../components/sideBar/sideBarMobile";
@@ -14,16 +14,17 @@ export const Home = () => {
     setOpen(!open);
   };
 
-  
   return (
     <>
       <div className={"h-full min-w-screen box-border"}>
         <Navbar />
         <Header />
-        <main className={`mt-12 min-h-full w-full container flex flex-col gap-4 relative box-border lg:flex-row`}>
-          <SideBar/>
-          <section className={"w-full h-full lg:w-full lg:min-h-max box-border"}>
-            <ul className={"flex lg:flex-wrap w-full gap-10 overflow-auto lg:justify-end"}>
+        <main
+          className={`mt-12 min-h-full w-full container flex flex-col gap-4 relative box-border lg:flex-row`}
+        >
+          <SideBar />
+          <section className={"w-full h-full lg:w-full lg:min-h-max box-border pb-5"}>
+            <ul className={"flex lg:flex-wrap w-full gap-10 overflow-auto lg:justify-end py-10"}>
               {carros.map((car) => (
                 <Card
                   id={car.id}
@@ -45,8 +46,12 @@ export const Home = () => {
           >
             Filtros
           </button>
-          
-          <div className={"flex absolute -bottom-32 gap-4 right-1/3 font-bold"}>
+
+          <div
+            className={
+              "flex absolute -bottom-24 right-20 sm:right-28 md:right-1/3 gap-4  font-bold"
+            }
+          >
             <span className={"text-2xl text-colorGreyScaleGrey3"}>
               1<span className={"text-colorGreyScaleGrey4"}> de 2</span>
             </span>
@@ -56,8 +61,7 @@ export const Home = () => {
           </div>
         </main>
       </div>
-        {open && <SideBarMobile setOpen={setOpen} />}
-      <div className={"mb-80"}></div>
+      {open && <SideBarMobile setOpen={setOpen} />}
       <Footer />
     </>
   );
