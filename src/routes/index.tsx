ac5b73d1bea85e92ecdf83d1c97ca01e25c19cd3
Dionisio <benevidescursos@gmail.com>
2023-06-16 13:19:24 -0300

    fix: adjusting logic render

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { DetailCar } from "../pages/DetailCar";
-
 import { Profile } from "../pages/Profile";
-
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 
