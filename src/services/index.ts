e9dbf98d625324331e817affca89f7bb54f01f3d
Dionisio <benevidescursos@gmail.com>
2023-07-05 19:00:19 -0300

    feat: adjusting api base url

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://webmotors-gp15.onrender.com/",
-  timeout: 5000,
+  timeout: 10000,
 });
 
 export const apiHerokuApp = axios.create({
